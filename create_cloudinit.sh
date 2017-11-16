492e9c0960d64aefe3ec85b32b5fef5548b81515
pblaas <patrick@kite4fun.nl>
2017-11-17 00:41:35 +0100

    update admin user certificate name

--- create_cloudinit.sh
+++ create_cloudinit.sh
@@ -73,7 +73,7 @@ done
 
 #create admin certs
 openssl genrsa -out admin-key.pem 2048
-openssl req -new -key admin-key.pem -out admin.csr -subj "/CN=cluster-admin:kube-admin"
+openssl req -new -key admin-key.pem -out admin.csr -subj "/CN=admin"
 openssl x509 -req -in admin.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out admin.pem -days 365
 
 #create demouser certs
