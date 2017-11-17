36d7bca13f8ef2f2e879c94aa17e85ab19cf0223
pblaas <patrick@kite4fun.nl>
2017-11-17 01:35:02 +0100

    change admin user group to system:masters

--- create_cloudinit.sh
+++ create_cloudinit.sh
@@ -73,7 +73,7 @@ done
 
 #create admin certs
 openssl genrsa -out admin-key.pem 2048
-openssl req -new -key admin-key.pem -out admin.csr -subj "/CN=kube-admin/O=cluster-admin"
+openssl req -new -key admin-key.pem -out admin.csr -subj "/CN=kube-admin/O=system:masters"
 openssl x509 -req -in admin.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out admin.pem -days 365
 
 #create demouser certs
